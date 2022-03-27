3b281920c607fae713e079f9b329a49fabc43936
sreejith <sreejithsev@gmail.com>
2022-03-27 11:30:17 +0530

    Add list of service to volunteer

--- src/main/java/com/hack/hackthonproject/domain/request/Volunteer.java
+++ src/main/java/com/hack/hackthonproject/domain/request/Volunteer.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 
 @Entity
@@ -26,4 +27,5 @@ public class Volunteer {
     private String phoneNumber;
     @NotNull
     private String emailId;
+    private List<String> services;
 }
